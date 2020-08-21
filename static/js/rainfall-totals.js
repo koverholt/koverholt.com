87d30b060f4be83c738e8866f19226cf134790da
Kristopher Overholt <kris@koverholt.com>
2020-08-21 15:01:11 -0500

    Update plot

--- static/js/rainfall-totals.js
+++ static/js/rainfall-totals.js
@@ -26,90 +26,65 @@ Algorithmia.client("simNv7chjBG6IS1nPZ9ocby/kEw1")
   var yValue = [previous_1_hour, previous_3_hours, previous_6_hours, previous_24_hours, since_midnight, one_day_ago, two_days_ago, three_days_ago, four_days_ago, five_day_total]
   var maxYValue = Math.max(...yValue)
 
-	var data = [
-    {
-      x: xValue,
-      y: yValue,
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myBarChart = new Chart(ctx, {
       type: 'bar',
-      text: yValue.map(String),
-      textposition: 'outside',
-    }
-  ];
 
-  var layout = {
-    yaxis: {range: [0, maxYValue + 0.5]},
-  };
+      data: {
+          labels: ["Last 1 hour",
+              "Last 3 hours",
+              "Last 6 hours",
+              "Last 24 hours",
+              "Since midnight",
+              "1 day ago",
+              "2 days ago",
+              "3 days ago",
+              "4 days ago",
+              "5 day total"
+          ],
+          datasets: [{
+              label: 'Rainfall Amount',
+              backgroundColor: 'rgb(3, 87, 255)',
+              borderColor: 'rgb(3, 87, 255)',
+              data: [previous_1_hour,
+                  previous_3_hours,
+                  previous_6_hours,
+                  previous_24_hours,
+                  since_midnight,
+                  one_day_ago,
+                  two_days_ago,
+                  three_days_ago,
+                  four_days_ago,
+                  five_day_total
+              ]
+          }]
+      },
 
-  var config = {
-    'displayModeBar': false
-  };
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              suggestedMin: 0,
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Rainfall Amount (in)'
+            },
+          }]
+        },
+      }
 
-  Plotly.newPlot('chart', data, layout, config);
-
-  Highcharts.chart('container', {
-      chart: {
-          type: 'column',
-      },
-      title: {
-          text: '',
-      },
-      credits: {
-        enabled: false,
-      },
-      xAxis: {
-          crosshair: true,
-      },
-      yAxis: {
-          min: 0,
-          title: {
-              text: 'Rainfall (in)',
-          }
-      },
-      tooltip: {
-          headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-              '<td style="padding:0"><b>{point.y:.2f} in</b></td></tr>',
-          footerFormat: '</table>',
-          shared: true,
-          useHTML: true
-      },
-      plotOptions: {
-          column: {
-              pointPadding: 0.2,
-              borderWidth: 0
-          }
-      },
-      series: [{
-          name: 'Previous 1 hour',
-          data: [previous_1_hour]
-      }, {
-          name: 'Previous 3 hours',
-          data: [previous_3_hours]
-      }, {
-          name: 'Previous 6 hours',
-          data: [previous_6_hours]
-      }, {
-          name: 'Previous 24 hours',
-          data: [previous_24_hours]
-      }, {
-          name: 'Since midnight',
-          data: [since_midnight]
-      }, {
-          name: '1 day ago',
-          data: [one_day_ago]
-      }, {
-          name: '2 days ago',
-          data: [two_days_ago]
-      }, {
-          name: '3 days ago',
-          data: [three_days_ago]
-      }, {
-          name: '4 days ago',
-          data: [four_days_ago]
-      }, {
-          name: '5 day total',
-          data: [five_day_total]
-      }]
   });
+
+  Chart.scaleService.updateScaleDefaults('linear', {
+    ticks: {
+        min: 0
+    }
+  });
+
+  Chart.defaults.global.defaultFontSize = 13;
 
   })
